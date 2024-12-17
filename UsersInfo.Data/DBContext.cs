2980b6e1e65be3ba954f646fef5e722faeee7aa5
Zmalki <zineddine.malki@tu-dortmund.de>
2024-12-17 15:22:00 +0100

    first project

---
+++ UsersInfo.Data/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UsersInfo.Core;
+
+namespace UsersInfo.Data
+{
+    public class DBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=.\SQLEXPRESS;Database=UserDemoDB;Encrypt=False; Trusted_Connection=true ");
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ UsersInfo.Data/IDataHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersInfo.Data
+{
+    internal interface IDataHelper<Table>
+    {
+        //Read
+        Task<List< Table>> GetAllAsync();
+        Task<List<Table>> SearchAsync(string Iteam);
+        Task<Table> FindAsync(int Id);
+        //Write  
+        Task addAsync(Table table); 
+        Task removeAsync(int Id );
+        Task EditAsync(Table table);
+
+    }
+}
